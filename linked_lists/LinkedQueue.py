63966f4ed94a09403bfdd12f14eb1261b7d53e8b
Alex Graf <alextgraf@gmail.com>
2020-10-19 17:18:24 -0700

    none

--- linked_lists/LinkedQueue.py
+++ linked_lists/LinkedQueue.py
@@ -6,17 +6,17 @@
 class LinkedQueue():
 	"""FIFO Queue using a singly linked list for storage"""
 	class _Node():
-		"""Linked List node - nested subclass"""
-		__slots__ = "_element","_next" # streamline memory usage
+	    """Linked List node - nested subclass"""
+	    __slots__ = "_element","_next" # streamline memory usage
         def __init__(self,element,nxt):
         	self._element = element
         	self._next = nxt
 
     # queue methods------------------    	
     def __init__(self):
-    	self._head = None
-    	self._tail = None
-    	self._size = 0
+        self._head = None
+        self._tail = None
+        self._size = 0
 
     def __len__(self):
         """Return the number of elements in the queue"""
@@ -24,18 +24,18 @@ class LinkedQueue():
 
     def is_empty(self):
     	"""Return true if the stack is empty"""
-    	return self._size == 0
+        return self._size == 0
 
     def first(self):
         """Return (but do not remove) the front of the queue"""
-    	if self.is_empty():
-    		raise Empty("queue is empty")
+        if self.is_empty():
+        	raise Empty("queue is empty")
         return self._head._element
 
     def dequeue(self):
         """Remove and return the first element of the queue"""
         if self.is_empty():
-    		raise Empty("queue is empty")
+            raise Empty("queue is empty")
         answer = self._head._element
         self._head = self._head._next
         self._size-=1
--- linked_lists/LinkedStack.py
+++ linked_lists/LinkedStack.py
@@ -4,27 +4,27 @@
 # Chapter 7 - Linked Lists
 
 class LinkedStack():
-	"""LIFO Stack using a singly linked list for storage"""
-	class _Node():
-		"""Linked List node - nested subclass"""
-		__slots__ = "_element","_next" # streamline memory usage
+    """LIFO Stack using a singly linked list for storage"""
+    class _Node():
+        """Linked List node - nested subclass"""
+        __slots__ = "_element","_next" # streamline memory usage
         def __init__(self,element,nxt):
         	self._element = element
         	self._next = nxt
 
     # stack methods------------------
     def __init__(self):
-    	"""Stack constructor"""
-    	self._head = None
-    	self._size = 0
+        """Stack constructor"""
+        self._head = None
+        self._size = 0
 
     def __len__(self):
     	"""Return the number of elements in the stack"""
-    	return self._size
+        return self._size
 
     def is_empty(self):
     	"""Return true if the stack is empty"""
-    	return self._size == 0
+        return self._size == 0
 
     def push(self,elem):
         """Add element to the top of the stack"""
@@ -32,17 +32,17 @@ class LinkedStack():
         self._size +=1
 
     def top(self):
-    	"""Return (but do not remove) the element at the top of the stack"""
-    	if self.is_empty():
-    		raise Empty("stack is empty")
-    	# top of the stack is at the head of the list
-    	return self._head._element
+        """Return (but do not remove) the element at the top of the stack"""
+        if self.is_empty():
+        	raise Empty("stack is empty")
+        # top of the stack is at the head of the list
+        return self._head._element
 
     def pop(self):
-    	"""Remove and return the element from the top of the stack"""
-    	if self.is_empty():
-    		raise Empty("stack is empty")
-    	answer = self._head._element
-    	self._head = self._head._next
-    	self._size-=1
-    	return answer
+        """Remove and return the element from the top of the stack"""
+        if self.is_empty():
+        	raise Empty("stack is empty")
+        answer = self._head._element
+        self._head = self._head._next
+        self._size-=1
+        return answer
