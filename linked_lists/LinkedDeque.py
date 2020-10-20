a25f0f744f9410c8e99151c662069f27f3fa1652
Alex Graf <alextgraf@gmail.com>
2020-10-19 17:13:24 -0700

    none

--- linked_lists/LinkedDeque.py
+++ linked_lists/LinkedDeque.py
@@ -6,33 +6,33 @@
 # requires importing DoublyLinkedBase class
 
 class LinkedDeque(DoublyLinkedBase):
-	"""Double-ended queue implementation based on a doubly linked list"""
-	def __init__(self):
-		pass
+    """Double-ended queue implementation based on a doubly linked list"""
+    def __init__(self):
+        pass
 
-	def first(self):
-		"""Return (but do not remove) the element at the front of the dequeue"""
-		if self.is_empty():
-			raise Empty("deque is empty")
-		return self._header._next._element
+    def first(self):
+        """Return (but do not remove) the element at the front of the dequeue"""
+        if self.is_empty():
+            raise Empty("deque is empty")
+        return self._header._next._element
 
-	def insert_first(self,elem):
-        """Add an element to the front of the deque"""
-        self._insert_between(elem,self._trailor._prev, self._header._next) # after header
+    def insert_first(self,elem):
+           """Add an element to the front of the deque"""
+           self._insert_between(elem,self._trailor._prev, self._header._next) # after header
 
     def insert_last(self,elem):
-    	"""Add an element to the back of the deque"""
-    	self._insert_between(elem,self._trailor._prev, self._trailor) # before trailor
+       	"""Add an element to the back of the deque"""
+       	self._insert_between(elem,self._trailor._prev, self._trailor) # before trailor
 
     def delete_first(self):
-    	"""Remove and return the element from the front of the deque"""
-		if self.is_empty():
-			raise Empty("deque is empty")    
-		return self._delete_node(self._header._next)	
+       	"""Remove and return the element from the front of the deque"""
+        if self.is_empty():
+            raise Empty("deque is empty")    
+        return self._delete_node(self._header._next)	
 
 
     def delete_last(self):
-    	"""Remove and return the element from the back of the deque"""
-		if self.is_empty():
-			raise Empty("deque is empty")    	
-		return self._delete_node(self._trailor._prev)	
+        """Remove and return the element from the back of the deque"""
+        if self.is_empty():
+            raise Empty("deque is empty")    	
+        return self._delete_node(self._trailor._prev)	
