ab07e310dc2dbaa9d785c40dd20b76c7734009f8
Alex Graf <alextgraf@gmail.com>
2020-10-19 17:14:40 -0700

    none

--- linked_lists/DoublyLinkedBase.py
+++ linked_lists/DoublyLinkedBase.py
@@ -5,10 +5,10 @@
 
 class DoublyLinkedBase():
 
-	class _Node():
-		"""Lightweight non-public class for storing a doubly linked node"""
-		__slots__ = "_element","_prev","_next" # streamline memory
-		def __init__(self,element,prev,next):
+    class _Node():
+        """Lightweight non-public class for storing a doubly linked node"""
+        __slots__ = "_element","_prev","_next" # streamline memory
+        def __init__(self,element,prev,next):
             self._element = element
             self._prev = prev
             self._next = next
@@ -23,24 +23,24 @@ class DoublyLinkedBase():
 
     def __len__(self):
     	"""Return the number of elements in the list"""
-    	return self._size
+        return self._size
 
     def is_empty(self):
     	"""Return True if list if empty"""
-    	return self._size == 0
+        return self._size == 0
 
     def _insert_between(self,elem,predecessor,successor):
     	"""Add element between two existing nodes and return new node"""
-    	newest = self._Node(elem,predecessor,successor)
-    	predecessor._next = newest
-    	successor._prev = newest
-    	self._size += 1
-    	return newest
+        newest = self._Node(elem,predecessor,successor)
+        predecessor._next = newest
+        successor._prev = newest
+        self._size += 1
+        return newest
 
     def _delete_node(self,node):
     	"""Delete nonsentinel node from the list and return its element"""
-    	predecessor = node._prev
-    	successor = node._next
+        predecessor = node._prev
+        successor = node._next
         predecessor._next = successor
         successor._prev = predecessor
         self._size-=1
