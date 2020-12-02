6c1a17a5e5d30927600f959a4db4890e194987fb
Alex Graf <alextgraf@gmail.com>
2020-12-02 12:32:05 -0800

    none

--- search_trees/RedBlackTreeMap.py
+++ search_trees/RedBlackTreeMap.py
@@ -6,4 +6,13 @@
 import TreeMap
 
 class RedBlackTreeMap(TreeMap):
-	
+	"""Sorted map implementation using a red black tree."""
+    class _Node(TreeMap._Node):
+        """Node class for red black tree maintains bit that denotes color."""
+        __slots__ = '_red'      # additional data member to the Node class
+
+        def __init__(self,element,parent=None,left=None,right=None):
+            super().__init__(element,parent,left,right)
+            self._red = True    # new node red by default
+
+            
