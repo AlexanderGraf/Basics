3bd4701ba96a5297f904b4d3397d0ed8b5702fb7
Alex Graf <alextgraf@gmail.com>
2020-12-03 16:45:14 -0800

    none

--- sorting_and_selection/quick_sort.py
+++ sorting_and_selection/quick_sort.py
@@ -33,3 +33,28 @@ def quick_sort(S):
         S.enqueue(E.dequeue())
     while not G.is_empty():
         S.enqueue(G.dequeue())
+
+# in place alternate version ---------------------------------------------------------
+def inplace_quick_sort(S,a,b):
+    """Sort the list from S[a] to S[b] inclusive using the quick-sort algo"""
+    if a>b: return              # range is trivially sorted
+    pivot = S[b]                # last element of the range is the pivot
+    left = a                    # will scan rightward
+    right = b-1                 # will scan leftward
+    while left <= right:
+        # scan until reaching value equal or larger than pivot (or right marker)
+        while left <= right and S[left] < pivot:
+            left+=1
+        # scan until reaching value equal or smaller than pivot (or left marker)
+        while left <= right and pivot < S[right]:
+            right-=1
+        if left <= right:                           # scans did not strictly cross
+            S[left], S[right] = S[right], S[left]   # swap values
+            left, right = left+1, right-1           # shrink range
+
+    # put pivot into it's final place (currently marked by left index)
+    S[left], S[b] = S[b], S[left]
+
+    # recurse
+    inplace_quick_sort(S,a,left-1)
+    inplace_quick_sort(S,left+1,b)
